5cb3237547d2910a8adf4c0d0c1d305a6e18fde3
IsanuelOrtiz <51845110+IsanuelOrtiz@users.noreply.github.com>
2024-09-09 18:16:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
      document.addEventListener('DOMContentLoaded', function() {
-    const audioPlayerContainer = document.getElementById('audioPlayerContainer');
-
-    audioPlayerContainer.innerHTML = `
-        <audio id="myAudio" autoplay loop>
-            <source src="Assets/Game Setting Battle (bddcb56b19db47c3a3d4972e574d659c).mp3" type="audio/mpeg">
-            Your browser does not support the audio element.
-        </audio>
-        <button id="playBtn" class="audio-btn">Pause</button>
-    `;
         const audio = document.getElementById("myAudio");
         const playBtn = document.getElementById("playBtn");
         
