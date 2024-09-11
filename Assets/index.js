e6049496aa374ac39c8895667d7b8f9940993a85
IsanuelOrtiz <51845110+IsanuelOrtiz@users.noreply.github.com>
2024-09-11 12:19:49 -0800

    Update index.js

--- Assets/index.js
+++ Assets/index.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var audio = document.getElementById("myAudio");
     var playBtn = document.getElementById("playBtn");
 
-}
-
     playBtn.addEventListener('click', toggleAudio);
 
     playBtn.innerHTML = audio.paused ? "Pause" : "Play";
