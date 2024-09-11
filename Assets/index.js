882b970d502c8d64c9e9d66a34b79c45efe9e751
IsanuelOrtiz <51845110+IsanuelOrtiz@users.noreply.github.com>
2024-09-11 12:50:44 -0800

    Update index.js

--- Assets/index.js
+++ Assets/index.js
@@ -2,6 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var audio = document.getElementById("myAudio");
     var playBtn = document.getElementById("playBtn");
 
+    function toggleAudio() {
+        if (audio.paused) {
+            audio.play();
+            playBtn.innerHTML = "Pause";
+        } else {
+            audio.pause();
+            playBtn.innerHTML = "Play";
+        }
+    }
+
     playBtn.addEventListener('click', toggleAudio);
 
     playBtn.innerHTML = audio.paused ? "Pause" : "Play";
